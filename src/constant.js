b55ead2103d9d2497c0929abcf6a43c5d630b45f
shubham465 <55125061+shubham465@users.noreply.github.com>
2025-11-10 14:33:55 +0000

    Update API domain constant and log the domain to console

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
-export const domain = import.meta.env.VITE_BASE_URL ?? 'http://localhost:8000';
+export const domain = import.meta.env.VITE_BASE_API_URL ?? 'http://localhost:8000';
+console.info('API Domain:', domain);
